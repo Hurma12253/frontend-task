5d7e05a5dcc71909fe8ccc20f39a5084ac27314a
Svyat <slava1511200@gmail.com>
2020-11-30 20:18:05 +0500

    added all the functionality of the order

--- src/components/Routes/Home/Order/OrderItem.tsx
+++ src/components/Routes/Home/Order/OrderItem.tsx
@@ -1,6 +1,9 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React, { FC } from 'react';
 import classes from '@styles/order.scss';
 import { observer } from 'mobx-react-lite';
+import dishesStore from '@stores/dishesStore';
 
 interface IOrderItem {
     children: Order;
@@ -8,13 +11,18 @@ interface IOrderItem {
 
 const OrderItem: FC<IOrderItem> = ({
     children: {
-        product: { name, photoUrl, priceInDollars },
+        product: { name, photoUrl, priceInDollars, id },
         qty,
     },
 }) => {
     return (
         <div className={classes.order__items__item}>
-            <img className={classes.order__items__item__delete} src="/assets/clear.svg" alt="clear" />
+            <img
+                className={classes.order__items__item__delete}
+                onClick={() => dishesStore.removeFromOrder(id)}
+                src="/assets/clear.svg"
+                alt="clear"
+            />
             <img className={classes.order__items__item__image} src={`http://localhost:3030${photoUrl}`} alt={name} />
             <div className={classes.order__items__item__body}>
                 <div className={classes.order__items__item__body__name}>{name}</div>
@@ -23,6 +31,7 @@ const OrderItem: FC<IOrderItem> = ({
             </div>
             <div className={classes.order__items__item__qty_container}>
                 <img
+                    onClick={() => dishesStore.addQty(id)}
                     className={classes.order__items__item__qty_container__up}
                     src="/assets/arrow_up.svg"
                     alt="arrow_up"
@@ -31,6 +40,7 @@ const OrderItem: FC<IOrderItem> = ({
                     <span className={classes.order__items__item__qty_container__qty__x}>x</span> {qty}
                 </div>
                 <img
+                    onClick={() => dishesStore.subQty(id)}
                     className={classes.order__items__item__qty_container__down}
                     src="/assets/arrow_up.svg"
                     alt="arrow_up"
--- src/stores/dishesStore.ts
+++ src/stores/dishesStore.ts
@@ -8,7 +8,7 @@ class DishesStore {
     constructor() {
         makeAutoObservable(this);
         this.dishes = [];
-        this.order = [];
+        this.order = JSON.parse(localStorage.getItem('Order') || '[]');
     }
 
     fetchDishes() {
@@ -27,15 +27,53 @@ class DishesStore {
     }
 
     addToOrder(id: string, qty: number) {
-        this.order.push({
-            product: this.dishes.find((el) => el.id === id) || {
-                id: 'none',
-                name: 'none',
-                photoUrl: 'none',
-                priceInDollars: 'none',
-            },
-            qty,
-        });
+        const product = this.dishes.find((el) => el.id === id);
+        const exists = this.order.find((el) => el.product.id === id);
+
+        if (product && !exists) {
+            this.order.push({ product, qty });
+        } else if (exists) {
+            this.addQty(id);
+        }
+
+        this.saveCart();
+    }
+
+    removeFromOrder(id: string) {
+        this.order = this.order.filter((x) => x.product.id !== id);
+        this.saveCart();
+    }
+
+    addQty(id: string) {
+        const index = this.order.findIndex((el) => el.product.id === id);
+
+        if (index !== -1) {
+            this.order[index].qty += 1;
+        }
+
+        this.saveCart();
+    }
+
+    subQty(id: string) {
+        const index = this.order.findIndex((el) => el.product.id === id);
+
+        if (index !== -1) {
+            this.order[index].qty -= 1;
+        }
+
+        if (this.order[index].qty < 1) {
+            this.removeFromOrder(id);
+        }
+
+        this.saveCart();
+    }
+
+    get sum() {
+        return this.order.reduce((acc, el) => acc + el.qty * +el.product.priceInDollars, 0).toFixed(2);
+    }
+
+    saveCart() {
+        localStorage.setItem('Order', JSON.stringify(this.order));
     }
 }
 
